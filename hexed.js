16fa9cf717f38d9b400116b264c61122a2a3665a
giovanni caniato <giocaniato@gmail.com>
2016-09-21 12:11:51 +0200

    mobile swipe ok

--- hexed.js
+++ hexed.js
@@ -13,6 +13,7 @@
         perspective: true,
         borders: false,
         bordersColor: '#000',
+        bordersThickness: 1,
         transitionTime: 0.5,
         transparentify: true,
         transparentOpacity: 0.45,
@@ -23,8 +24,9 @@
 
       $.extend(_.options, settings);
 
-      if (_.options.borders) _.roundFun = function(n) { return n };
-      else                   _.roundFun = Math.floor;
+      //if (_.options.borders) _.roundFun = function(n) { return n };
+      //else                   _.roundFun = Math.floor;
+      _.roundFun = function(n) { return n };
 
       _.instanceId = instanceId++;
       _.$hexed = $(element);
@@ -42,9 +44,11 @@
       _.mainTransform = '';
 
       _.swiping = false;
+      _.touching = false;
       _.clickThreshold = 5;
       _.startX = null;
       _.startY = null;
+      _.touchData = {};
 
       _.init();
     }
@@ -93,17 +97,20 @@
         height = _.$hexed.height(),
         w =  _.roundFun(height / 2.0),
         w2 = _.roundFun(w / 2.0),
-        xd = 0,
+        xd = 0.0,
         yd = _.roundFun(0.75 * w),
         zd = _.roundFun(0.433013 * w),
-        zdPlus = 0,
-        persp = 0;
+        zdPlus = 0.0,
+        persp = 0.0;
 
     if (_.options.perspective) {
       persp = height*5;
       zdPlus = height/5*1.2;
     }
-    if (_.options.borders) xd = 1;
+    if (_.options.borders) {
+      xd = _.options.bordersThickness;
+      _.$hexed.css({ margin: xd+'px' });
+    }
     _.$hexed.cssPrefix({ perspective: persp+'px' });
     _.mainTransform = 'translateX('+(-xd)+'px) translateY('+(w2-xd)+'px) translateZ('+(-zd-zdPlus)+'px)';
     main.cssPrefix({
@@ -116,12 +123,12 @@
       });
     }, 60);
 
-    $('<div class="hexed-side hexed-side0">').appendTo(main).css(sideCSS(w, -yd,    zd,   60));
-    $('<div class="hexed-side hexed-side1">').appendTo(main).css(sideCSS(w,   0,  2*zd,    0));
-    $('<div class="hexed-side hexed-side2">').appendTo(main).css(sideCSS(w,  yd,    zd,  -60));
-    $('<div class="hexed-side hexed-side3">').appendTo(main).css(sideCSS(w,  yd,   -zd, -120));
-    $('<div class="hexed-side hexed-side4">').appendTo(main).css(sideCSS(w,   0, -2*zd,  180));
-    $('<div class="hexed-side hexed-side5">').appendTo(main).css(sideCSS(w, -yd,   -zd,  120));
+    $('<div class="hexed-side">').appendTo(main).css(sideCSS(w, -yd,    zd,   60));
+    $('<div class="hexed-side">').appendTo(main).css(sideCSS(w,   0,  2*zd,    0));
+    $('<div class="hexed-side">').appendTo(main).css(sideCSS(w,  yd,    zd,  -60));
+    $('<div class="hexed-side">').appendTo(main).css(sideCSS(w,  yd,   -zd, -120));
+    $('<div class="hexed-side">').appendTo(main).css(sideCSS(w,   0, -2*zd,  180));
+    $('<div class="hexed-side">').appendTo(main).css(sideCSS(w, -yd,   -zd,  120));
   }
 
 
@@ -155,7 +162,7 @@
         $(_.sides[_.currentSide]).css({ opacity: 1.0 });
       }
       if (_.options.borders)
-        _.sides.css({ border: '1px solid ' + _.options.bordersColor });
+        _.sides.css({ border: _.options.bordersThickness+'px solid ' + _.options.bordersColor });
 
       _.sides.addClass('hexed-no-interaction');
       if (_.options.currentChoiceModifiable)
@@ -180,76 +187,81 @@
 
     _.$hexed.on('mousedown touchstart',   { action: 'start' }, _.swipeHandler);
     _.$hexed.on('mouseup touchend',       { action: 'end' },   _.swipeHandler);
-    _.$hexed.on('mouseleave touchcancel', { action: 'out' },   _.swipeHandler);
+    _.$hexed.on('mouseleave touchcancel', { action: 'end' },   _.swipeHandler);
+    _.$hexed.on('touchmove', $.proxy(_.storeTouchPosition, _));
   }
+
 
   Hexed.prototype.swipeHandler = function(event) {
 
-    var _ = this;
+    var _ = this,
+        touches = event.originalEvent !== undefined ? event.originalEvent.touches : null,
+        posX = touches && touches.length === 1 ? touches[0].pageX : event.pageX || _.touchData.pageX,
+        posY = touches && touches.length === 1 ? touches[0].pageY : event.pageY || _.touchData.pageY;
 
-    if (event.data.action == 'start') {
-      _.startX = event.pageX;
-      _.startY = event.pageY;
+    if (touches && touches.length > 1) return false;
+
+    if (event.data.action === 'start') {
+      _.startX = posX;
+      _.startY = posY;
       _.swiping = true;
     }
     else if (_.swiping) {
-      var posX = event.pageX,
-          posY = event.pageY,
-          distX = posX - _.startX,
+      var distX = posX - _.startX,
           distY = posY - _.startY,
           dist = Math.sqrt(Math.pow(distX,2) + Math.pow(distY,2));
 
-      if (dist <= _.clickThreshold) {
-        var y = _.startY - _.$hexed.offset().top;
-        if      (y < _.height * 0.25) _.rotateDown()
-        else if (y > _.height * 0.75) _.rotateUp();
+      if (dist <= _.clickThreshold && !touches) {
+        var y = _.startY - _.$hexed.offsetFix().top;
+
+        if      (y < _.height * 0.25) _.rotate('down')
+        else if (y > _.height * 0.75) _.rotate('up');
       }
       else if (Math.abs(distY) > Math.abs(distX)) {
-        if (distY > 0) _.rotateDown();
-        else           _.rotateUp();
+        if (distY > 0) _.rotate('down');
+        else           _.rotate('up');
       }
 
+      _.touchData = {};
       _.swiping = false;
     }
   }
 
 
-  Hexed.prototype.rotateDown = function() {
+  Hexed.prototype.storeTouchPosition = function(event) {
+    var _ = this,
+        touches = event.originalEvent.touches;
 
-    var _ = this;
+    if (touches && touches.length == 1) {
+      event.preventDefault();
+      _.touchData.pageX = touches[0].pageX;
+      _.touchData.pageY = touches[0].pageY;
+    }
+  }
+
+
+  Hexed.prototype.rotate = function(direction) {
+
+    var _ = this, rotation;
+
+    if (direction === 'up')        rotation = 1;
+    else if (direction === 'down') rotation = -1;
+    else return false;
 
     if (_.options.lazyUpdate)
-      _.setSide(_.currentSide-2, _.currentChoice-2);
+      _.setSide(_.currentSide + rotation*2, _.currentChoice + rotation*2);
 
-    _.rotation--;
+    _.rotation += rotation;
     _.previousSide = _.currentSide;
-    _.currentSide = mod(_.currentSide-1, 6);
-    _.currentChoice = mod(_.currentChoice-1, _.choicesNum);
+    _.currentSide = mod(_.currentSide + rotation, 6);
+    _.currentChoice = mod(_.currentChoice + rotation, _.choicesNum);
 
     if (! _.options.lazyUpdate)
-      _.setSide(_.currentSide-2, _.currentChoice-2);
+      _.setSide(_.currentSide + rotation*2, _.currentChoice + rotation*2);
 
     _.updateHexagon();
   }
 
-
-  Hexed.prototype.rotateUp = function() {
-
-    var _ = this;
-
-    if (_.options.lazyUpdate)
-      _.setSide(_.currentSide+2, _.currentChoice+2);
-
-    _.rotation++;
-    _.previousSide = _.currentSide;
-    _.currentSide = mod(_.currentSide+1, 6);
-    _.currentChoice = mod(_.currentChoice+1, _.choicesNum);
-
-    if (! _.options.lazyUpdate)
-      _.setSide(_.currentSide+2, _.currentChoice+2);
-
-    _.updateHexagon();
-  }
 
   Hexed.prototype.updateHexagon = function() {
 
@@ -267,6 +279,14 @@
 
   function mod(x, n) {
     return (((x%n)+n) % n);
+  }
+
+  $.fn.offsetFix = function() {
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+    if (isChrome)
+      return { top: this.get(0).offsetTop, left: this.get(0).offsetLeft };
+    return this.offset();
   }
 
   $.fn.cssPrefix = function(obj) {
